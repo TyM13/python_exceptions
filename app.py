cd7627ed2d1453f4756b58276b701aab8806d9cb
Your Name <you@example.com>
2022-09-22 19:45:40 -0600

    didn't save

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ try:
     z = x + y
 except TypeError:
     print("sorry you can't add a string to an int")
+except:
+    print('something bad has happened get help')
+finally:
+    print('I will always happen, error or no error')
